7f5f35dbe957eed8124c90fc5c6c00403fd120e1
xize <glucassen@live.nl>
2018-01-17 16:09:49 +0100

    verify if my commits are signed now.

---
+++ flyingcake.php
@@ -0,0 +1,24 @@
+<?php
+
+$chakes = array(
+	'I',
+	'\'',
+	'm',
+	's',
+	'i',
+	'g',
+	'n',
+	'e',
+	'd',
+	'?',
+	':',
+	'D'
+);
+
+$text = "";
+
+foreach($cakes as $cake) {
+	$text .= $cake;
+}
+
+echo $text;
